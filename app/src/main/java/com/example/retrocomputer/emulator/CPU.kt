901a6629675bb37cf2720ed815f90c6c8902c139
ZuzRad <107064508+ZuzRad@users.noreply.github.com>
2022-12-07 16:40:45 +0100

    Update CPU.kt

--- app/src/main/java/com/example/retrocomputer/emulator/CPU.kt
+++ app/src/main/java/com/example/retrocomputer/emulator/CPU.kt
@@ -131,9 +131,9 @@ class CPU() {
 
     enum class Opcode {
         XXX, ADC, AND, ASL, BIT, BPL, BMI, BVC, BVS, BCC, BCS, BNE,
-//        BEQ, BRK, CMP, CPX, CPY, DEC, EOR, CLC,
-//        SEC, CLI, SEI, CLV, CLD, SED, INC, JMP, JSR, LDA, LDX, LDY, LSR, NOP, ORA, TAX, TXA, DEX, INX,
-//        TAY, TYA, DEY, INY, ROR, ROL, RTI, RTS, SBC, STA, TXS, TSX, PHA, PLA, PHP, PLP, STX, STY
+        BEQ, BRK, CMP, CPX, CPY, DEC, EOR, CLC,
+        SEC, CLI, SEI, CLV, CLD, SED, INC, JMP, JSR, LDA, LDX, LDY, LSR, NOP, ORA, TAX, TXA, DEX, INX,
+        TAY, TYA, DEY, INY, ROR, ROL, RTI, RTS, SBC, STA, TXS, TSX, PHA, PLA, PHP, PLP, STX, STY
     }
 
     private fun handleOpcodes(opcode: Opcode): UByte {
@@ -150,51 +150,51 @@ class CPU() {
             Opcode.BCC -> BCC()
             Opcode.BCS -> BCS()
             Opcode.BNE -> BNE()
-//            Opcode.BEQ -> BEQ()
-//            Opcode.BRK -> BRK()
-//            Opcode.CMP -> CMP()
-//            Opcode.CPX -> CPX()
-//            Opcode.CPY -> CPY()
-//            Opcode.DEC -> DEC()
-//            Opcode.EOR -> EOR()
-//            Opcode.CLC -> CLC()
-//            Opcode.SEC -> SEC()
-//            Opcode.CLI -> CLI()
-//            Opcode.SEI -> SEI()
-//            Opcode.CLV -> CLV()
-//            Opcode.CLD -> CLD()
-//            Opcode.SED -> SED()
-//            Opcode.INC -> INC()
-//            Opcode.JMP -> JMP()
-//            Opcode.JSR -> JSR()
-//            Opcode.LDA -> LDA()
-//            Opcode.LDX -> LDX()
-//            Opcode.LDY -> LDY()
-//            Opcode.LSR -> LSR()
-//            Opcode.NOP -> NOP()
-//            Opcode.ORA -> ORA()
-//            Opcode.TAX -> TAX()
-//            Opcode.TXA -> TXA()
-//            Opcode.DEX -> DEX()
-//            Opcode.INX -> INX()
-//            Opcode.TAY -> TAY()
-//            Opcode.TYA -> TYA()
-//            Opcode.DEY -> DEY()
-//            Opcode.INY -> INY()
-//            Opcode.ROR -> ROR()
-//            Opcode.ROL -> ROL()
-//            Opcode.RTI -> RTI()
-//            Opcode.RTS -> RTS()
-//            Opcode.SBC -> SBC()
-//            Opcode.STA -> STA()
-//            Opcode.TXS -> TXS()
-//            Opcode.TSX -> TSX()
-//            Opcode.PHA -> PHA()
-//            Opcode.PLA -> PLA()
-//            Opcode.PHP -> PHP()
-//            Opcode.PLP -> PLP()
-//            Opcode.STX -> STX()
-//            Opcode.STY -> STY()
+            Opcode.BEQ -> BEQ()
+            Opcode.BRK -> BRK()
+            Opcode.CMP -> CMP()
+            Opcode.CPX -> CPX()
+            Opcode.CPY -> CPY()
+            Opcode.DEC -> DEC()
+            Opcode.EOR -> EOR()
+            Opcode.CLC -> CLC()
+            Opcode.SEC -> SEC()
+            Opcode.CLI -> CLI()
+            Opcode.SEI -> SEI()
+            Opcode.CLV -> CLV()
+            Opcode.CLD -> CLD()
+            Opcode.SED -> SED()
+            Opcode.INC -> INC()
+            Opcode.JMP -> JMP()
+            Opcode.JSR -> JSR()
+            Opcode.LDA -> LDA()
+            Opcode.LDX -> LDX()
+            Opcode.LDY -> LDY()
+            Opcode.LSR -> LSR()
+            Opcode.NOP -> NOP()
+            Opcode.ORA -> ORA()
+            Opcode.TAX -> TAX()
+            Opcode.TXA -> TXA()
+            Opcode.DEX -> DEX()
+            Opcode.INX -> INX()
+            Opcode.TAY -> TAY()
+            Opcode.TYA -> TYA()
+            Opcode.DEY -> DEY()
+            Opcode.INY -> INY()
+            Opcode.ROR -> ROR()
+            Opcode.ROL -> ROL()
+            Opcode.RTI -> RTI()
+            Opcode.RTS -> RTS()
+            Opcode.SBC -> SBC()
+            Opcode.STA -> STA()
+            Opcode.TXS -> TXS()
+            Opcode.TSX -> TSX()
+            Opcode.PHA -> PHA()
+            Opcode.PLA -> PLA()
+            Opcode.PHP -> PHP()
+            Opcode.PLP -> PLP()
+            Opcode.STX -> STX()
+            Opcode.STY -> STY()
         }
     }
 
@@ -232,6 +232,141 @@ class CPU() {
         return 0U
     }
     private fun BNE() : UByte {
+        return 0U
+    }
+    private fun BEQ() : UByte {
+        return 0U
+    }
+    private fun BRK() : UByte {
+        return 0U
+    }
+    private fun CMP() : UByte {
+        return 0U
+    }
+    private fun CPX() : UByte {
+        return 0U
+    }
+    private fun CPY() : UByte {
+        return 0U
+    }
+    private fun DEC() : UByte {
+        return 0U
+    }
+    private fun EOR() : UByte {
+        return 0U
+    }
+    private fun CLC() : UByte {
+        return 0U
+    }
+    private fun SEC() : UByte {
+        return 0U
+    }
+    private fun CLI() : UByte {
+        return 0U
+    }
+    private fun SEI() : UByte {
+        return 0U
+    }
+    private fun CLV() : UByte {
+        return 0U
+    }
+    private fun CLD() : UByte {
+        return 0U
+    }
+    private fun SED() : UByte {
+        return 0U
+    }
+    private fun INC() : UByte {
+        return 0U
+    }
+    private fun JMP() : UByte {
+        return 0U
+    }
+    private fun JSR() : UByte {
+        return 0U
+    }
+    private fun LDA() : UByte {
+        return 0U
+    }
+    private fun LDX() : UByte {
+        return 0U
+    }
+    private fun LDY() : UByte {
+        return 0U
+    }
+    private fun LSR() : UByte {
+        return 0U
+    }
+    private fun NOP() : UByte {
+        return 0U
+    }
+    private fun ORA() : UByte {
+        return 0U
+    }
+    private fun TAX() : UByte {
+        return 0U
+    }
+    private fun TXA() : UByte {
+        return 0U
+    }
+    private fun DEX() : UByte {
+        return 0U
+    }
+    private fun INX() : UByte {
+        return 0U
+    }
+    private fun TAY() : UByte {
+        return 0U
+    }
+    private fun TYA() : UByte {
+        return 0U
+    }
+    private fun DEY() : UByte {
+        return 0U
+    }
+    private fun INY() : UByte {
+        return 0U
+    }
+    private fun ROR() : UByte {
+        return 0U
+    }
+    private fun ROL() : UByte {
+        return 0U
+    }
+    private fun RTI() : UByte {
+        return 0U
+    }
+    private fun RTS() : UByte {
+        return 0U
+    }
+    private fun SBC() : UByte {
+        return 0U
+    }
+    private fun STA() : UByte {
+        return 0U
+    }
+    private fun TXS() : UByte {
+        return 0U
+    }
+    private fun TSX() : UByte {
+        return 0U
+    }
+    private fun PHA() : UByte {
+        return 0U
+    }
+    private fun PLA() : UByte {
+        return 0U
+    }
+    private fun PHP() : UByte {
+        return 0U
+    }
+    private fun PLP() : UByte {
+        return 0U
+    }
+    private fun STX() : UByte {
+        return 0U
+    }
+    private fun STY() : UByte {
         return 0U
     }
 
