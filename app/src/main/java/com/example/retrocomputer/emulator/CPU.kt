94748e73f83816ab342b9910eb19d6d9eae7829d
loyyo <57000021+loyyo@users.noreply.github.com>
2022-12-07 16:07:44 +0100

    Update CPU.kt

--- app/src/main/java/com/example/retrocomputer/emulator/CPU.kt
+++ app/src/main/java/com/example/retrocomputer/emulator/CPU.kt
@@ -9,7 +9,7 @@ class CPU() {
     var Y: UByte = 0x00U      // Register Y
     var SP: UByte = 0x00U  // Stack pointer
     var PC: UShort = 0x0000U // Program counter
-    var status: UByte = 0x00U
+    var status: UByte = 0x00U // Status register (current flag)
 
 //    Flags
     var flagC : Boolean = false  // Carry Bit
@@ -29,4 +29,117 @@ class CPU() {
     var flagShiftU = (1 shl 5)
     var flagShiftV = (1 shl 6)
     var flagShiftN = (1 shl 7)
+
+//    Functions / Other Pins
+
+    private fun clock() {
+
+    }
+    private fun reset() {
+
+    }
+    private fun irq() {     // Interrupt Request
+
+    }
+    private fun nmi() {     // Non-Maskable Interrupt
+
+    }
+
+    private fun fetch () : UByte {
+        return 0U
+    }
+
+    var fetched : UByte = 0x00U
+
+    var addr_abs : UShort = 0x0000U
+    var addr_rel : UShort = 0x00U
+    var opcode : UByte = 0x00U
+    var cycles : UByte = 0U
+
+//    Addressing modes
+
+    enum class AddressingMode {
+        IMM, // Immediate
+        IMP, // Implied
+        ZP0, // Zero Page absolute
+        ZPX, // Zero Page X
+        ZPY, // Zero Page Y
+        REL,  // Relative
+        ABS, // Absolute A
+        ABX, // Absolute X
+        ABY, // Absolute Y
+        IND, // Indirect
+        IZX, // Pre-indexed indirect
+        IZY, // Post-indexed indirect
+    }
+
+    private fun handleAddressingMode(addrMode: AddressingMode): UByte {
+        return when(addrMode) {
+            AddressingMode.IMM -> IMM()
+            AddressingMode.IMP -> IMP()
+            AddressingMode.ZP0 -> ZP0()
+            AddressingMode.ZPX -> ZPX()
+            AddressingMode.ZPY -> ZPY()
+            AddressingMode.REL -> REL()
+            AddressingMode.ABS -> ABS()
+            AddressingMode.ABX -> ABX()
+            AddressingMode.ABY -> ABY()
+            AddressingMode.IND -> IND()
+            AddressingMode.IZX -> IZX()
+            AddressingMode.IZY -> IZY()
+        }
+    }
+
+    private fun IMM() : UByte {
+        return 0U
+    }
+    private fun IMP() : UByte {
+       return 0U
+    }
+    private fun ZP0() : UByte {
+        return 0U
+    }
+    private fun ZPX() : UByte {
+        return 0U
+    }
+    private fun ZPY() : UByte {
+        return 0U
+    }
+    private fun REL() : UByte {
+        return 0U
+    }
+    private fun ABS() : UByte {
+        return 0U
+    }
+    private fun ABX() : UByte {
+        return 0U
+    }
+    private fun ABY() : UByte {
+        return 0U
+    }
+    private fun IND() : UByte {
+        return 0U
+    }
+    private fun IZX() : UByte {
+        return 0U
+    }
+    private fun IZY() : UByte {
+        return 0U
+    }
+
+//    Opcodes
+    enum class Opcodes {
+        ADC, AND, ASL, BIT, BPL, BMI, BVC, BVS, BCC, BCS, BNE, BEQ, BRK, CMP, CPX, CPY, DEC, EOR, CLC,
+        SEC, CLI, SEI, CLV, CLD, SED, INC, JMP, JSR, LDA, LDX, LDY, LSR, NOP, ORA, TAX, TXA, DEX, INX,
+        TAY, TYA, DEY, INY, ROR, ROL, RTI, RTS, SBC, STA, TXS, TSX, PHA, PLA, PHP, PLP, STX, STY, XXX
+    }
+
+    data class Instruction (
+        val name: String = "???",
+        val opcode: UByte,
+        val mode: AddressingMode,
+        val cycles: UByte = 0U
+    )
+
+    var lookup = List<Instruction>(0x100) { i -> i. }
 }
