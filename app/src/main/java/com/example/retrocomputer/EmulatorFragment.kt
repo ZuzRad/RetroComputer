2bc0fce6bf68f7a5d81e8b7530aa9db321e8d29f
ZuzRad <zuzarad785@student.polsl.pl>
2022-12-10 13:20:49 +0100

    poprawka

--- app/src/main/java/com/example/retrocomputer/EmulatorFragment.kt
+++ app/src/main/java/com/example/retrocomputer/EmulatorFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.retrocomputer.databinding.FragmentEmulatorBinding
 
 class EmulatorFragment : Fragment() {
@@ -22,6 +24,7 @@ class EmulatorFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val myButton = view.findViewById<Button>(R.id.butt_uruchom)
@@ -33,9 +36,11 @@ class EmulatorFragment : Fragment() {
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
             requireActivity().title = "Disassembler"
+
         }
 
-        val edittext= view.findViewById<EditText>(R.id.textView3)
+       val edittext= view.findViewById<EditText>(R.id.textView3)
+
         val myButton1 = view.findViewById<Button>(R.id.butt1)
         myButton1.setOnClickListener{
             edittext.setText("test1")
@@ -51,16 +56,39 @@ class EmulatorFragment : Fragment() {
             edittext.setText("test3")
         }
 
-        val myButtonWczytaj = view.findViewById<Button>(R.id.butt_wczytaj)
-        myButtonWczytaj.setOnClickListener{
-            pickTxt()
-        }
+
+
+
+
+//        val getTxt = registerForActivityResult(
+//            ActivityResultContracts.GetContent(),
+//            ActivityResultCallback {
+//
+//
+//            }
+//        )
+//        val myButtonWczytaj = view.findViewById<Button>(R.id.butt_wczytaj)
+//        myButtonWczytaj.setOnClickListener{
+//            getTxt.launch("image/*")
+//        }
     }
 
-    private fun pickTxt(){
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "application/txt"
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == 100) {
+//            edittext.setText(data.data)
+//        }
+//    }
 
-    }
+
+
+//        val intent = Intent()
+//            .setType("application/txt")
+//            .setAction(Intent.ACTION_GET_CONTENT)
+//
+//        startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
+
+
 
 }
