8f2d7975a4f7563e0cd0fd8d71de3b0dd89a3da6
ZuzRad <zuzarad785@student.polsl.pl>
2022-12-16 21:31:57 +0100

    testy oraz wygląd

--- app/src/main/java/com/example/retrocomputer/EmulatorFragment.kt
+++ app/src/main/java/com/example/retrocomputer/EmulatorFragment.kt
@@ -51,17 +51,101 @@ class EmulatorFragment : Fragment() {
 
         val myButton1 = view.findViewById<Button>(R.id.butt1)
         myButton1.setOnClickListener{
-            edittext.setText("test1")
+            assembly = "LDA #$48\n" +
+                    "STA $0000\n" +
+                    "LDA #$45\n" +
+                    "STA $0001\n" +
+                    "LDA #$4C\n" +
+                    "STA $0002\n" +
+                    "LDA #$4C\n" +
+                    "STA $0003\n" +
+                    "LDA #$4F\n" +
+                    "STA $0004\n" +
+                    "LDA #$20\n" +
+                    "STA $0005\n" +
+                    "LDA #$57\n" +
+                    "STA $0006\n" +
+                    "LDA #$4F\n" +
+                    "STA $0007\n" +
+                    "LDA #$52\n" +
+                    "STA $0008\n" +
+                    "LDA #$4C\n" +
+                    "STA $0009\n" +
+                    "LDA #$44\n" +
+                    "STA $000A"
+            edittext.setText(assembly)
         }
 
         val myButton2 = view.findViewById<Button>(R.id.butt2)
         myButton2.setOnClickListener{
-            edittext.setText("test2")
+            assembly ="LDX #$28\n" +
+                    "STX $0000\n" +
+                    "LDX #$30\n" +
+                    "STX $0001\n" +
+                    "LDX #$5F\n" +
+                    "STX $0002\n" +
+                    "LDX #$30\n" +
+                    "STX $0003\n" +
+                    "LDX #$29\n" +
+                    "STX $0004\n" +
+                    "LDX #$20\n" +
+                    "STX $0005\n" +
+                    "LDX #$3C\n" +
+                    "STX $0006\n" +
+                    "LDX #$28\n" +
+                    "STX $0007\n" +
+                    "LDX #$5E\n" +
+                    "STX $0008\n" +
+                    "LDX #$5F\n" +
+                    "STX $0009\n" +
+                    "LDX #$5E\n" +
+                    "STX $000A\n" +
+                    "LDX #$29\n" +
+                    "STX $000B\n" +
+                    "LDX #$3E\n" +
+                    "STX $000C\n" +
+                    "LDX #$20\n" +
+                    "STX $000D\n" +
+                    "LDX #$28\n" +
+                    "STX $000E\n" +
+                    "LDX #$3D\n" +
+                    "STX $000F\n" +
+                    "LDX #$5F\n" +
+                    "STX $0010\n" +
+                    "LDX #$3D\n" +
+                    "STX $0011\n" +
+                    "LDX #$29\n" +
+                    "STX $0012"
+            edittext.setText(assembly)
         }
 
         val myButton3 = view.findViewById<Button>(R.id.butt3)
         myButton3.setOnClickListener{
-            edittext.setText("test3")
+            assembly = "LDY #\$3A\n" +
+                    "STY \$0000\n" +
+                    "LDY #\$29\n" +
+                    "STY \$0001\n" +
+                    "LDY #\$20\n" +
+                    "STY \$0002\n" +
+                    "LDY #\$3A\n" +
+                    "STY \$0003\n" +
+                    "LDY #\$2F\n" +
+                    "STY \$0004\n" +
+                    "LDY #\$20\n" +
+                    "STY \$0005\n" +
+                    "LDY #\$3A\n" +
+                    "STY \$0006\n" +
+                    "LDY #\$44\n" +
+                    "STY \$0007\n" +
+                    "LDY #\$20\n" +
+                    "STY \$0008\n" +
+                    "LDY #\$2E\n" +
+                    "STY \$0009\n" +
+                    "LDY #\$5F\n" +
+                    "STY \$000A\n" +
+                    "LDY #\$2E\n" +
+                    "STY \$000B"
+            edittext.setText(assembly)
         }
 
 
