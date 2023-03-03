4c56041d5eee8d7a0921131635984f3b2fc2c7c7
ZuzRad <zuzarad785@student.polsl.pl>
2023-03-03 23:37:20 +0100

    first

--- app/src/main/java/com/example/retrocomputer/emulator/Instruction.kt
+++
@@ -1,9 +0,0 @@
-package com.example.retrocomputer
-
-// Struktura instrukcji
-data class Instruction (
-    val name: String,
-    val opcode: Opcode,
-    val mode: AddressingMode,
-    val cycles: Int,
-)
